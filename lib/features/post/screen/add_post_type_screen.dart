60b0006c3194375ab25f233c0942c6943e6e3d7c
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-02-21 23:06:11 +0530

    add post type screen ui added

---
+++ lib/features/post/screen/add_post_type_screen.dart
@@ -0,0 +1,190 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/common/error.dart';
+import '../../../core/common/loader.dart';
+import '../../../core/utils.dart';
+import '../../../model/community_model.dart';
+import '../../../theme/pallete.dart';
+import '../../community/controller/community_controller.dart';
+
+class AddPostTypeScreen extends ConsumerStatefulWidget {
+  final String type;
+
+  const AddPostTypeScreen({
+    super.key,
+    required this.type,
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _AddPostTypeScreenState();
+}
+
+class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final linkController = TextEditingController();
+
+  File? bannerFile;
+  Uint8List? bannerWebFile;
+  List<Community> communities = [];
+  Community? selectedCommunity;
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    descriptionController.dispose();
+    linkController.dispose();
+  }
+
+  void selectBannerImage() async {
+    final res = await pickImage();
+
+    if (res != null) {
+      if (kIsWeb) {
+        setState(
+          () {
+            bannerWebFile = res.files.first.bytes;
+          },
+        );
+      }
+      setState(
+        () {
+          bannerFile = File(res.files.first.path!);
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isTypeImage = widget.type == 'image';
+    final isTypeText = widget.type == 'text';
+    final isTypeLink = widget.type == 'link';
+    final currentTheme = ref.watch(themeNotifierProvider);
+    // final isLoading = ref.watch(postControllerProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Post ${widget.type}'),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Share'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(
+                filled: true,
+                hintText: 'Enter Title here',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 30,
+            ),
+            const SizedBox(height: 10),
+            if (isTypeImage)
+              GestureDetector(
+                onTap: selectBannerImage,
+                child: DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  color: currentTheme.textTheme.bodyMedium!.color!,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: bannerWebFile != null
+                        ? Image.memory(bannerWebFile!)
+                        : bannerFile != null
+                            ? Image.file(bannerFile!)
+                            : const Center(
+                                child: Icon(
+                                  Icons.camera_alt_outlined,
+                                  size: 40,
+                                ),
+                              ),
+                  ),
+                ),
+              ),
+            if (isTypeText)
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: 'Enter Description here',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+                maxLines: 5,
+              ),
+            if (isTypeLink)
+              TextField(
+                controller: linkController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: 'Enter link here',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+              ),
+            const SizedBox(height: 20),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Select Community',
+              ),
+            ),
+            ref.watch(userCommunitiesProvider).when(
+                  data: (data) {
+                    communities = data;
+
+                    if (data.isEmpty) {
+                      return const SizedBox();
+                    }
+                    return DropdownButton(
+                      value: selectedCommunity ?? data[0],
+                      items: data
+                          .map(
+                            (e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(e.name),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (val) {
+                        setState(
+                          () {
+                            selectedCommunity = val;
+                          },
+                        );
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) => ErrorText(
+                    error: error.toString(),
+                  ),
+                  loading: () => const Loader(),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
